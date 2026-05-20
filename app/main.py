63412376d003a4d63350fd2288e7ac9f23beff5c
Jim M <77047005+Jim2578@users.noreply.github.com>
2026-05-20 09:49:13 +0200

    core(main): add versions

--- app/main.py
+++ app/main.py
@@ -5,7 +5,8 @@ app = FastAPI()
 def root_endpooint():
     return {
         "message": "allo le monde",
-        "status": "ok"
+        "status": "ok",
+        "version": "3.0.0"
     }
 
 def hello():
